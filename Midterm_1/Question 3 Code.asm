78ed192210cca3f8a5e6ab95263ffba1ed616686
L10NH34RT <zleo050907@gmail.com>
2025-07-03 12:08:26 -0700

    Update Question 3 Code.asm

--- Midterm_1/Question 3 Code.asm
+++ Midterm_1/Question 3 Code.asm
@@ -7,3 +7,34 @@ section .data
 section .text
     global _start
 
+_start:
+    mov eax, [number]           
+    
+    mov ebx, 2                 
+    cdq                         
+    idiv ebx                   
+                               
+    cmp edx, 0                 
+    je print_even               
+    jmp print_odd               
+    
+print_even:
+    mov eax, 4                 
+    mov ebx, 1                  
+    mov ecx, msg_even          
+    mov edx, 12                
+    int 0x80
+    jmp exit_program
+    
+print_odd:
+    mov eax, 4                  
+    mov ebx, 1                  
+    mov ecx, msg_odd           
+    mov edx, 11                 
+    int 0x80
+    
+
+exit_program:
+    mov eax, 1                 
+    mov ebx, 0                  
+    int 0x80
